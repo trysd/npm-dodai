5139d178318c6c81cb5d0320b08bfcb6fee29253
trysd <itrysd@icloud.com>
2023-01-07 02:22:58 +0900

    feat: rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,20 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
+import { terser as pluginTerser } from "rollup-plugin-terser";
 
-export default {
+export default [
+  {
   input: 'src/index.ts',
   output: {
     dir: 'dist',
-    format: 'cjs'
+    format: 'cjs',
+
+    // minify用プラグインを追加で実行する
+    plugins: [
+      pluginTerser(),
+    ],
   },
   plugins: [typescript()]
-};
+}
+
+];
